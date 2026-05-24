4bace8f562dbc499dfe8c1daf120be9681c2847c
ailefabi <ailenfabi25semi@hotmail.com>
2026-05-24 15:04:28 -0600

    Se agrega el proyecto SpringBoot al repositorio

---
+++ backend/MercadU/src/main/java/ucr/ac/cr/MercadU/MercadUApplication.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.MercadU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadUApplication.class, args);
+	}
+
+}
---
+++ backend/MercadU/src/test/java/ucr/ac/cr/MercadU/MercadUApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.MercadU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
